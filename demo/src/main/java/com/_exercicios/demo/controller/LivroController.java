90d98c84e7ab1f6051f76806011b8996754b68e0
João Paulo <joaopaulotraguetta14@gmail.com>
2026-03-18 20:22:33 -0300

    livroControllet

--- demo/src/main/java/com/_exercicios/demo/controller/LivroController.java
+++ demo/src/main/java/com/_exercicios/demo/controller/LivroController.java
@@ -1,9 +1,11 @@
 package com._exercicios.demo.controller;
 
+import com._exercicios.demo.models.LivroModel;
 import com._exercicios.demo.services.LivroService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/livros")
@@ -12,4 +14,24 @@ public class LivroController {
 
     @Autowired
     public LivroService livroService;
+
+    @GetMapping
+    public List<LivroModel> findAll(){
+        return livroService.listarLivros();
+    }
+
+    @PostMapping
+    public LivroModel save(@RequestBody LivroModel livroModel){
+        return livroService.criarLivro(livroModel);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+        livroService.deletarLivro(id);
+    }
+
+    @GetMapping
+    public LivroModel findById(@RequestParam Long id){
+        return livroService.findById(id);
+    }
 }
